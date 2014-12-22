b0bfa34bd9bd2160f6e707db0ea521c225fada85
Jim Noellsch <jnoellsch@rightpoint.com>
2014-12-21 19:52:27 -0600

    Added test for AddFilter.

--- Src/Ektron.SharedSource.FluentApi.Tests/FilterContentCollectionTests.cs
+++ Src/Ektron.SharedSource.FluentApi.Tests/FilterContentCollectionTests.cs
@@ -1,6 +1,11 @@
 ﻿namespace Ektron.SharedSource.FluentApi.Tests
 {
+    using Ektron.Cms.Common;
+    using Ektron.Cms.Content;
+
     using NUnit.Framework;
+
+    using Ploeh.AutoFixture.NUnit2;
 
     public class FilterContentCollectionTests
     {
@@ -11,6 +16,24 @@
             public void HasGuardClauses()
             {
                 this.GuardClauseAssertion.Verify(typeof(FilterContentCollection).GetConstructors());
+            }
+        }
+
+        [TestFixture]
+        public class AddFilterMethod
+        {
+            [Test, AutoData]
+            public void AddsToCriteriaFilter(string value)
+            {
+                var field = ContentCollectionProperty.Description;
+                var criteria = new ContentCollectionCriteria();
+                var sut = new FilterContentCollection(criteria, field);
+
+                sut.Contains(value);
+
+                Assert.AreEqual(1, criteria.CollectionGroupFilters.Count);
+                Assert.AreEqual(field, criteria.CollectionGroupFilters[0].Filters[0].Field);
+                Assert.AreEqual(value, criteria.CollectionGroupFilters[0].Filters[0].Value);
             }
         }
     }
