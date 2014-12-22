ab7a130ad4f63a0e7374cae59a25b87b587bf20b
ssartell <ssartell@rightpoint.com>
2014-12-22 08:34:25 -0600

    Add ContentData extensions

---
+++ Src/Ektron.SharedSource.FluentApi/ContentDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ektron.Cms;
+
+namespace Ektron.SharedSource.FluentApi
+{
+    public static class ContentDataExtensions
+    {
+        public static IEnumerable<T> AsContentType<T>(this IEnumerable<ContentData> source)
+        {
+            return source.Select(c => c.AsContentType<T>());
+        }
+
+        public static T AsContentType<T>(this ContentData source)
+        {
+            return (T)EkXml.Deserialize(typeof(T), source.Html);
+        }
+    }
+}
