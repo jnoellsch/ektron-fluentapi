bb5ccd0b3530ff7133f83e0b53a18b9dffc7310a
Jim Noellsch <jnoellsch@rightpoint.com>
2014-12-22 21:54:28 -0600

    Added FolderCriteriaExtensions.

---
+++ Src/Ektron.SharedSource.FluentApi.Tests/FolderCriteriaTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Ektron.SharedSource.FluentApi.Tests
+{
+    using Ektron.Cms;
+    using Ektron.Cms.Common;
+
+    using NUnit.Framework;
+
+    using Ploeh.AutoFixture.NUnit2;
+
+    public class FolderCriteriaTests
+    {
+        [TestFixture]
+        public class ByFolderMethod
+        {
+            [Test, AutoData]
+            public void SetsIdFilterIfLong(long id)
+            {
+                var sut = new FolderCriteria().ByFolder(id);
+
+                Assert.AreEqual(1, sut.FilterGroups.Count);
+                Assert.AreEqual(FolderProperty.Id, sut.FilterGroups[0].Filters[0].Field);
+                Assert.AreEqual(CriteriaFilterOperator.EqualTo, sut.FilterGroups[0].Filters[0].Operator);
+                Assert.AreEqual(id, sut.FilterGroups[0].Filters[0].Value);
+            }
+
+            [Test, AutoData]
+            public void SetsPathFilterIfString(string path)
+            {
+                var sut = new FolderCriteria().ByFolder(path);
+
+                Assert.AreEqual(1, sut.FilterGroups.Count);
+                Assert.AreEqual(FolderProperty.FolderPath, sut.FilterGroups[0].Filters[0].Field);
+                Assert.AreEqual(CriteriaFilterOperator.EqualTo, sut.FilterGroups[0].Filters[0].Operator);
+                Assert.AreEqual(path, sut.FilterGroups[0].Filters[0].Value);
+            }
+        }
+
+        [TestFixture]
+        public class WithChildPropertiesMethod
+        {
+            [Test]
+            public void SetsWithChildPropertiesPropertyToTrue()
+            {
+                var sut = new FolderCriteria().WithChildProperties();
+                Assert.IsTrue(sut.ReturnChildProperties);
+            }
+        }
+    }
+}
---
+++ Src/Ektron.SharedSource.FluentApi/FolderCriteriaExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace Ektron.SharedSource.FluentApi
+{
+    using Ektron.Cms;
+    using Ektron.Cms.Common;
+
+    /// <summary>
+    /// Represents a set of extensions for <see cref="FolderCriteria"/>.
+    /// </summary>
+    public static class FolderCriteriaExtensions
+    {
+        /// <summary>
+        /// Adds an exact match filter for <see cref="FolderProperty.Id"/>.
+        /// </summary>
+        /// <param name="criteria">The criteria to extend.</param>
+        /// <param name="id">The folder id.</param>
+        /// <returns>The updated criteria.</returns>
+        public static FolderCriteria ByFolder(this FolderCriteria criteria, long id)
+        {
+            criteria.FilteredBy(FolderProperty.Id).EqualTo(id);
+            return criteria;
+        }
+
+        /// <summary>
+        /// Adds an exact match filter for <see cref="FolderProperty.FolderPath"/>.
+        /// </summary>
+        /// <param name="criteria">The criteria to extend.</param>
+        /// <param name="path">The folder path.</param>
+        /// <returns>The updated criteria.</returns>
+        public static FolderCriteria ByFolder(this FolderCriteria criteria, string path)
+        {
+            criteria.FilteredBy(FolderProperty.FolderPath).EqualTo(path);
+            return criteria;
+        }
+
+        /// <summary>
+        /// Sets the <see cref="FolderCriteria.ReturnChildProperties"/> property to true.
+        /// Ths informs the query to return folder details.
+        /// </summary>
+        /// <param name="criteria">The criteria to extend.</param>
+        /// <returns>The updated criteria.</returns>
+        public static FolderCriteria WithChildProperties(this FolderCriteria criteria)
+        {
+            criteria.ReturnChildProperties = true;
+            return criteria;
+        }
+
+        /// <summary>
+        /// Adds a filter option.
+        /// </summary>
+        /// <param name="criteria">The criteria to extend.</param>
+        /// <param name="field">The field to filter on.</param>
+        /// <returns>An instance of <see cref="FilterFolder"/>, which further refines the operator.</returns>
+        public static FilterFolder FilteredBy(this FolderCriteria criteria, FolderProperty field)
+        {
+            return new FilterFolder(criteria, field);
+        }
+    }
+}
