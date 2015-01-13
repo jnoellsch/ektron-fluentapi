33cca5f7192fe1f01b93544463c925f306dae0a5
ssartell <ssartell@rightpoint.com>
2015-01-12 20:44:45 -0600

    Add unit test for FilterTaxonomy class

---
+++ Src/Ektron.SharedSource.FluentApi.Tests/FilterTaxonomyTests.cs
@@ -0,0 +1,48 @@
+﻿using Ektron.Cms.Organization;
+
+namespace Ektron.SharedSource.FluentApi.Tests
+{
+    using Ektron.Cms.Common;
+    using Ektron.Cms.Content;
+
+    using NUnit.Framework;
+
+    using Ploeh.AutoFixture.NUnit2;
+
+    public class FilterTaxonomyTests
+    {
+        [TestFixture]
+        public class Constructor : TestBase
+        {
+            [Test]
+            public void HasGuardClauses()
+            {
+                this.GuardClauseAssertion.Verify(typeof(FilterTaxonomy).GetConstructors());
+            }
+
+            [Test]
+            public void SetsProperties()
+            {
+                this.ConstructorInitializedMemberAssertion.Verify(typeof(FilterTaxonomy).GetConstructors());
+            }
+        }
+
+        [TestFixture]
+        public class AddFilterMethod
+        {
+            [Test, AutoData]
+            public void AddsToCriteriaFilter(string value)
+            {
+                var field = TaxonomyProperty.Path;
+                var criteria = new TaxonomyCriteria();
+                var sut = new FilterTaxonomy(criteria, field);
+
+                sut.Contains(value);
+
+                Assert.AreEqual(1, criteria.FilterGroups.Count);
+                Assert.AreEqual(field, criteria.FilterGroups[0].Filters[0].Field);
+                Assert.AreEqual(value, criteria.FilterGroups[0].Filters[0].Value);
+            }
+        }
+    }
+}
