36b5d48a61e17a2514c6f8d32f1c8c70d3a8a380
DrewNull <dnull@rightpoint.com>
2015-01-15 09:29:13 -0600

    Began adding search criteria tests.

--- Src/Ektron.SharedSource.FluentApi.Tests/SearchCriteriaTests.cs
+++ Src/Ektron.SharedSource.FluentApi.Tests/SearchCriteriaTests.cs
@@ -1,6 +1,49 @@
 ﻿namespace Ektron.SharedSource.FluentApi.Tests
 {
+    using System.Linq;
+
+    using Ektron.Cms.Search;
+
+    using NUnit.Framework;
+
     public class SearchCriteriaTests
     {
+        [TestFixture]
+        public class EnableRefinementMethod
+        {
+            [Test]
+            public void SetsEnableRefinementPropertyToTrue()
+            {
+                var sut = new KeywordSearchCriteria().EnableRefinement();
+                Assert.IsTrue(sut.Refinement.IsEnabled);
+            }
+        }
+
+        [TestFixture]
+        public class RefineByMethod
+        {
+            [Test]
+            public void AddsPropertyAsRefinement()
+            {
+                var property = SearchContentProperty.Title;
+                var criteria = new KeywordSearchCriteria()
+                    .RefineBy(SearchContentProperty.Title);
+                var firstRefinement = criteria.Refinement.Items.FirstOrDefault();
+                Assert.IsNotNull(firstRefinement);
+                Assert.AreEqual(
+                    firstRefinement.Property, 
+                    property);
+            }
+        }
+
+        [TestFixture]
+        public class RefineMultiValueByMethod
+        {
+        }
+
+        [TestFixture]
+        public class ReturnsWithMethod
+        {
+        }
     }
 }
