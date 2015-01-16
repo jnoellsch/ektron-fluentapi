750b9e51b4b1fd459b3e2540b54686cb18b19d50
ssartell <ssartell@rightpoint.com>
2015-01-15 20:45:47 -0600

    Rename smart form mapping attributes

--- Src/Ektron.SharedSource.FluentApi/Mapping/Attributes/SmartFormFieldValueAttribute.cs
+++ Src/Ektron.SharedSource.FluentApi/Mapping/Attributes/SmartFormFieldValueAttribute.cs
@@ -5,19 +5,19 @@ namespace Ektron.SharedSource.FluentApi.Mapping.Attributes
     /// <summary>
     /// Attribute indicating the name of the Smart Form field to find the value.
     /// </summary>
-    public class SmartFormComplexAttribute : Attribute
+    public class SmartFormFieldValueAttribute : Attribute
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SmartFormComplexAttribute"/> class.
+        /// Initializes a new instance of the <see cref="SmartFormFieldValueAttribute"/> class.
         /// </summary>
         /// <param name="xpath">The xpath.</param>
-        public SmartFormComplexAttribute(string xpath)
+        public SmartFormFieldValueAttribute(string xpath)
         {
             this.Xpath = xpath;
         }
 
         /// <summary>
-        /// Gets the xpath the indicating the node with the complex type to be used.
+        /// Gets the xpath the indicating the node with the value to be used.
         /// </summary>
         public string Xpath { get; private set; }
     }
--- Src/Ektron.SharedSource.FluentApi/Mapping/Attributes/SmartFormObjectAttribute.cs
+++ Src/Ektron.SharedSource.FluentApi/Mapping/Attributes/SmartFormObjectAttribute.cs
@@ -5,19 +5,19 @@ namespace Ektron.SharedSource.FluentApi.Mapping.Attributes
     /// <summary>
     /// Attribute indicating the name of the Smart Form field to find the value.
     /// </summary>
-    public class SmartFormPrimitiveAttribute : Attribute
+    public class SmartFormObjectAttribute : Attribute
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SmartFormPrimitiveAttribute"/> class.
+        /// Initializes a new instance of the <see cref="SmartFormObjectAttribute"/> class.
         /// </summary>
         /// <param name="xpath">The xpath.</param>
-        public SmartFormPrimitiveAttribute(string xpath)
+        public SmartFormObjectAttribute(string xpath)
         {
             this.Xpath = xpath;
         }
 
         /// <summary>
-        /// Gets the xpath the indicating the node with the value to be used.
+        /// Gets the xpath the indicating the node with the complex type to be used.
         /// </summary>
         public string Xpath { get; private set; }
     }
